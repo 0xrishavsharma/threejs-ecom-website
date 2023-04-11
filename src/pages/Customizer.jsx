38091e3bffc98ac0bf3156af36de4b405311c7f6
Rishav Sharma <rishavtosharma@gmail.com>
2023-04-11 23:07:04 +0530

    feat: :lipstick: added necessary pages, configurations, components and started working on the main functionality

---
+++ src/pages/Customizer.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store'
+import config from "../config/config"
+import { download } from "../assets"
+import { downloadCanvasToImage, reader } from "../config/helpers"
+
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion"
+
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+
+const Customizer = () => {
+    const snap = useSnapshot(state);
+    return (
+
+        <AnimatePresence>
+            {
+                !snap.isHome && (
+                    <>
+                        <motion.div
+                            key="custom"
+                            className="customizer"
+                            {...slideAnimation('left')}
+                        >
+                            <div className='flex items-center min-h-screen'>
+                                <div className='editortabs-container tabs'>
+                                    {
+                                        EditorTabs.map((tab, index) => {
+                                            return (
+                                                <Tab
+                                                    key={tab.name}
+                                                    tab={tab}
+                                                    handleClick={() => { }}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </motion.div>
+                        <motion.div
+                            className='absolute z-10 top-5 right-5'
+                            {...fadeAnimation}
+                        >
+                            <CustomButton
+                                type="filled"
+                                title="Go Back"
+                                handleClick={() => state.isHome = true}
+                            />
+                        </motion.div>
+                    </>
+                )
+
+            }
+        </AnimatePresence>
+    )
+}
+
+export default Customizer
