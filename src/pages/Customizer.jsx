5e4777caaa8ebc59eeaf25c054941ecff25cea00
Rishav Sharma <rishavtosharma@gmail.com>
2023-04-12 12:20:38 +0530

    feat: :lipstick: added the camera, initialised it and defined it's values in different position

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -22,22 +22,18 @@ const Customizer = () => {
                     <>
                         <motion.div
                             key="custom"
-                            className="customizer"
+                            className="absolute top-0 left-0 z-10"
                             {...slideAnimation('left')}
                         >
-                            <div className='flex items-center min-h-screen'>
-                                <div className='editortabs-container tabs'>
-                                    {
-                                        EditorTabs.map((tab, index) => {
-                                            return (
-                                                <Tab
-                                                    key={tab.name}
-                                                    tab={tab}
-                                                    handleClick={() => { }}
-                                                />
-                                            )
-                                        })
-                                    }
+                            <div className="flex items-center min-h-screen">
+                                <div className="editortabs-container tabs">
+                                    {EditorTabs.map((tab) => (
+                                        <Tab
+                                            key={tab.name}
+                                            tab={tab}
+                                            handleClick={() => { }}
+                                        />
+                                    ))}
                                 </div>
                             </div>
                         </motion.div>
