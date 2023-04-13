413620ef267beef3a90b327b1200168d060ab2a0
Rishav Sharma <rishavtosharma@gmail.com>
2023-04-13 23:13:31 +0530

    feat: :lipstick: almost done with threejs camera for the project

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -13,6 +13,15 @@ const Shirt = () => {
     const logoTexture = useTexture(snap.logoDecal);
     const fullTexture = useTexture(snap.fullDecal);
 
+    // We will be playing with easing for a bit to apply color smoothly and not dramatically
+    useFrame((state, delta) => {
+        easing.dampC(materials.lambert1.color,
+            snap.color,
+            0.25,
+            delta
+        )
+    })
+
     return (
         <group>
             <mesh
@@ -20,15 +29,28 @@ const Shirt = () => {
                 geometry={nodes.T_Shirt_male.geometry}
                 material={materials.lambert1}
                 material-roughness={1}
-                dispose={null}
+                dispose={null} // to avoid memory leaks
             >
                 {
                     snap.isFullTexture && (
                         <Decal
                             position={[0, 0, 0]}
                             rotation={[0, 0, 0]}
-                            scale={1}
-                            map={fullTexture}
+                            scale={1} // the size of the decal
+                            map={fullTexture} // the texture
+                        />
+                    )
+                }
+                {
+                    snap.isLogoTexture && (
+                        <Decal
+                            position={[0, 0.04, 0.15]}
+                            rotation={[0, 0, 0]}
+                            scale={0.15} // the size of the decal
+                            map={logoTexture} // the texture
+                            map-anisotropy={16} // to change the quality of the texture
+                            depthTest={false} // to avoid z-fighting, when true the logo will be displayed on top of every layer but here we only want it to be on top of shirt
+                            depthWrite={true}
                         />
                     )
                 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { proxy } from "valtio";
 const state = proxy({
     isHome: true,
     color: '#5e00d8',
-    isLogoTexture: false,
+    isLogoTexture: true,
     isFullTexture: false,
     logoDecal: "./threejs.png",
     fullDecal: "./threejs.png",
